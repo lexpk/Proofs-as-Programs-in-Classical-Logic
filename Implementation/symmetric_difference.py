7613aef571b61d9ba48f7348b4cb14d5a52d0fb7
Alexander Pluska <alexander.pluska@gmail.com>
2022-10-13 23:22:40 +0200

    Symmetric difference

--- Implementation/symmetric_difference.py
+++ Implementation/symmetric_difference.py
@@ -1,5 +1,12 @@
 import os
 
+from itertools import chain, combinations
+
+def powerset(iterable):
+    "list(powerset([1,2,3])) --> [(), (1,), (2,), (3,), (1,2), (1,3), (2,3), (1,2,3)]"
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
 import tqdm
 
 RESULT_ROOT = "./ILTP-v1.1.2-firstorder-results"
@@ -18,26 +25,11 @@ COUNTERMODEL = {
     "-nanocop" : "is an intuitionistic Non-Theorem"
 }
 
-buckets = {0 : [0, 2, 5, 1000], 2 : [2, 5, 1000], 5 : [5, 1000], 1000 : [1000] }
-tally = {}
+result = {tuple(provers) : 0 for provers in powerset(PROVERS)}
 
 for directory in os.listdir(TRANSLATION_ROOT):
     for translation in tqdm.tqdm(os.listdir(os.path.join(TRANSLATION_ROOT, directory)), desc=f"{directory}"):
-        translation_path = os.path.join(os.path.join(
-            TRANSLATION_ROOT, directory), translation)
-        with open(translation_path, "r") as translation_text:
-            try:
-                translation_text = translation_text.read()
-                q = translation_text.count("a,axiom,'f_!")
-                x = (translation_text.count("a,axiom,'f") - q)//2
-            except MemoryError:
-                q = 100
-                x = 100
-        i = min([b for b in buckets.keys() if b >= q])
-        j = min([b for b in buckets[i] if b >= x])
-        if not (i, j) in tally:
-            tally[(i, j)] = {p : 0 for p in PROVERS + ["total"]}
-        tally[(i, j)]["total"] += 1
+        solvedby = []
         for p in PROVERS:
             root = RESULT_ROOT + p
             result_path = os.path.join(os.path.join(root, directory), translation)
@@ -45,10 +37,10 @@ for directory in os.listdir(TRANSLATION_ROOT):
                 with open(result_path, "r") as result_text:
                     result_text = result_text.read()
                 if PROOF[p] in result_text or COUNTERMODEL[p] in result_text:
-                    tally[(i, j)][p] += 1
-            except FileNotFoundError:
+                    solvedby.append(p)
+            except:
                 pass
-for k, v in tally.items():
+        result[tuple(solvedby)] += 1
+
+for k, v in result.items():
     print(f"{k}: {v}")
-for k, v in tally.items():
-    print(f"{k}: {dict((l, v/tally[k]['total']) for (l, v) in tally[k].items())}")
